61874399c2df6e449c1bc95eff64a65e6d7cad6d
Tom Jemmett <thomas.jemmett@nhs.net>
2022-08-26 09:48:13 +0100

    fixes ip data extraction methods

--- targets/R/get_ip_data.R
+++ targets/R/get_ip_data.R
@@ -8,7 +8,7 @@ get_ip_dsr_data <- function(provider_successors_last_updated, catchments, lkp_eu
   tbl_age_table <- dplyr::tbl(con, dbplyr::in_schema("nhp_modelling_reference", "age_groups"))
   tbl_ip_strategies <- dplyr::tbl(con, dbplyr::in_schema("nhp_modelling", "strategies_grouped"))
 
-  dplyr::tbl(con, dbplyr::in_schema("nhp_modelling", "inpatients")) |>
+  data <- dplyr::tbl(con, dbplyr::in_schema("nhp_modelling", "inpatients")) |>
     dplyr::inner_join(tbl_provider_successors, by = c("PROCODE3" = "old_code")) |>
     dplyr::mutate(PROCODE3 = .data$new_code) |>
     dplyr::filter(.data$sex %in% c(1, 2)) |>
@@ -17,20 +17,12 @@ get_ip_dsr_data <- function(provider_successors_last_updated, catchments, lkp_eu
     dplyr::group_by(.data$FYEAR, .data$age_group, .data$SEX, .data$PROCODE3, .data$strategy) |>
     dplyr::summarise(n = n(), dplyr::across(.data$sample_rate, sum, na.rm = TRUE), .groups = "drop") |>
     dplyr::collect() |>
-    janitor::clean_names() |>
+    janitor::clean_names()
+
+  data |>
     dplyr::left_join(catchments, by = c("fyear", "sex", "age_group", "procode3" = "provider")) |>
     dplyr::mutate(dplyr::across(.data$pop_catch, tidyr::replace_na, 0)) |>
-    dplyr::inner_join(lkp_euro_2013, by = c("sex", "age_group")) |>
-    dplyr::group_by(.data$strategy, .data$fyear, .data$procode3) |>
-    dplyr::mutate(
-      rate_local = .data$n / .data$pop_catch,
-      std_number = .data$rate_local * .data$pop_euro
-    ) |>
-    dplyr::summarise(
-      std_rate = sum(.data$std_number) / sum(.data$pop_euro),
-      .groups = "drop"
-    ) |>
-    dplyr::arrange(strategy, fyear, procode3)
+    dplyr::inner_join(lkp_euro_2013, by = c("sex", "age_group"))
 }
 
 get_ip_diag_data <- function(provider_successors_last_updated) {
@@ -56,9 +48,10 @@ get_ip_diag_data <- function(provider_successors_last_updated) {
     dplyr::inner_join(tbl_ip_strategies, by = c("EPIKEY")) |>
     dplyr::group_by(.data$FYEAR, .data$PROCODE3, .data$strategy, .data$DIAGNOSIS) |>
     dplyr::summarise(n = n(), .groups = "drop_last") |>
-    dplyr::mutate(p = .data$n / sum(.data$n, na.rm = TRUE)) |>
+    dplyr::mutate(p = .data$n * 1.0 / sum(.data$n, na.rm = TRUE)) |>
     dbplyr::window_order(dplyr::desc(.data$n)) |>
     dplyr::filter(dplyr::row_number() <= 6) |>
     dplyr::ungroup() |>
-    dplyr::collect()
+    dplyr::collect() |>
+    janitor::clean_names()
 }
