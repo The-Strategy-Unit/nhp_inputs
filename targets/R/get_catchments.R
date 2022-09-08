0945acef538d76cd35cc7c1014b693d23c54b837
Tom Jemmett <thomas.jemmett@nhs.net>
2022-09-08 15:15:28 +0100

    updates query removing need for successors table

--- targets/R/get_catchments.R
+++ targets/R/get_catchments.R
@@ -1,26 +1,23 @@
 get_catchments <- function(provider_successors_last_updated, pop_year_long) {
   con <- get_con("HESData")
 
-  tbl_provider_successors <- dplyr::tbl(con, dbplyr::in_schema("nhp_modelling", "provider_successors"))
-
   tbl_age_table <- dplyr::tbl(con, dbplyr::in_schema("nhp_modelling_reference", "age_groups"))
 
   dplyr::tbl(con, dbplyr::in_schema("nhp_modelling", "inpatients")) |>
     dplyr::filter(.data$LSOA11 %LIKE% "E%") |>
     dplyr::inner_join(tbl_age_table, by = c("ADMIAGE" = "age")) |>
-    dplyr::inner_join(tbl_provider_successors, by = c("PROCODE3" = "old_code")) |>
     dplyr::group_by(
       .data$FYEAR,
       .data$LSOA11,
       .data$age_group,
       .data$SEX
     ) |>
-    dplyr::count(PROCODE3 = .data$new_code) |>
+    dplyr::count(.data$PROCODE3) |>
+    dplyr::collect() |>
     dplyr::mutate(
       tot = sum(.data$n, na.rm = TRUE),
       p = n * 1.0 / .data$tot
     ) |>
-    dplyr::collect() |>
     dplyr::ungroup() |>
     janitor::clean_names() |>
     dplyr::inner_join(
