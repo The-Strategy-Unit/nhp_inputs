0bc1958c4a391d352e1adaecc81baaf10066a2c8
Tom Jemmett <thomas.jemmett@nhs.net>
2022-09-14 11:21:07 +0100

    adds home  module

--- R/app_server.R
+++ R/app_server.R
@@ -10,13 +10,9 @@ app_server <- function(input, output, session) {
   providers <- readRDS(app_sys("app", "data", "providers.Rds"))
   strategies <- readRDS(app_sys("app", "data", "strategies.Rds"))
 
-  selected_provider <- shiny::reactive({
-    "RL4" # todo: convert to an input
-  })
-
-  selected_baseline_year <- shiny::reactive({
-    201819 # todo: create an input
-  })
+  home_module <- mod_home_server("home", providers, peers)
+  selected_provider <- shiny::reactive(shiny::req(home_module()$provider))
+  selected_baseline_year <- shiny::reactive(shiny::req(home_module()$baseline))
 
   mod_mitigators_admission_avoidance_server(
     "mitigators_admission_avoidance",
