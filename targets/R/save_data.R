10e5e55514233eff1ab1d0d4b73c9269769c2ff9
Tom Jemmett <thomas.jemmett@nhs.net>
2022-09-29 10:45:04 +0100

    updates save data method to only save where all data files are available

--- targets/R/save_data.R
+++ targets/R/save_data.R
@@ -3,12 +3,7 @@ save_data <- function(...) {
 
   list(...) |>
     purrr::imap(\(data, key) dplyr::group_nest(data, .data$procode, .data$strategy, .key = key)) |>
-    purrr::reduce(dplyr::full_join, by = c("procode", "strategy")) |>
-    # make sure we have rows for the age_sex and diagnoses
-    dplyr::rowwise() |>
-    dplyr::filter(!(is.null(.data$age_sex) & is.null(.data$diagnoses))) |>
-    dplyr::ungroup() |>
-    #
+    purrr::reduce(dplyr::inner_join, by = c("procode", "strategy")) |>
     tidyr::pivot_longer(!where(rlang::is_atomic)) |>
     dplyr::filter(!purrr::map_lgl(.data$value, is.null)) |>
     dplyr::group_nest(.data$procode) |>
