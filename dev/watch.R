46c93ccfaad442351c72832879bf58b7e795d53a
Tom Jemmett <thomas.jemmett@nhs.net>
2022-10-08 13:21:11 +0100

    alters the new watch task

--- dev/watch.R
+++ dev/watch.R
@@ -1,6 +1,4 @@
-get_max_modification_time <- function(...) {
-  files <- fs::dir_ls(recurse = TRUE, ...)
-
+get_max_modification_time <- function(files) {
   max(fs::file_info(files)$modification_time)
 }
 
@@ -8,7 +6,12 @@ get_max_modification_time <- function(...) {
 previous_max_time <- 0
 task <- list(kill = \() TRUE)
 while (TRUE) {
-  new_max_time <- get_max_modification_time(path = c("R"), glob = "*.R")
+  new_max_time <- c(
+    fs::dir_ls(path = c("R"), recurse = TRUE, glob = "*.R"),
+    "inst/golem-config.yml",
+    "DESCRIPTION"
+  ) |>
+    get_max_modification_time()
 
   if (new_max_time > previous_max_time) {
     cat(format(Sys.time(), "%Y-%m-%d %H:%M:%S"), ": restarting app\n", sep = "")
