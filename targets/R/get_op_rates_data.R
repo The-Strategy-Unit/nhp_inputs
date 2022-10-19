ff34e6127d97eff23ad47e3db1e334af305043bd
Tom Jemmett <thomas.jemmett@nhs.net>
2022-10-19 14:06:43 +0100

    alters data extract for op diag/age+sex data

--- targets/R/get_op_rates_data.R
+++ targets/R/get_op_rates_data.R
@@ -45,25 +45,14 @@ get_op_data <- function(provider_successors_last_updated) {
 get_op_diag_data <- function(provider_successors_last_updated) {
   force(provider_successors_last_updated)
 
-  strategies <- purrr::cross_df(
-    list(
-      strategy = c(
-        "convert_to_tele",
-        "consultant_to_consultant_reduction",
-        "followup_reduction"
-      ),
-      subgroup = c(
-        "adult_non-surgical",
-        "adult_surgical",
-        "child_surgical",
-        "child_non-surgical"
-      )
-    )
+  strategies <- tibble::tribble(
+    ~name, ~strategy,
+    "is_cons_cons_ref", "consultant_to_consultant_reduction",
+    "is_first", "followup_reduction",
+    "is_tele_appointment", "convert_to_tele"
   )
 
   con <- get_con("HESData")
-
-  tbl_outpatients <- dplyr::tbl(con, dbplyr::in_schema("nhp_modelling", "outpatients"))
 
   tbl_outpatients_diagnoses <- dplyr::tbl(con, dbplyr::in_schema("nhp_modelling", "outpatients_diagnoses")) |>
     dplyr::filter(.data$diagorder == 1) |>
@@ -71,22 +60,49 @@ get_op_diag_data <- function(provider_successors_last_updated) {
       dplyr::across("diagnosis", LEFT, 3)
     )
 
-  tbl_outpatients |>
+  tbl_outpatients <- dplyr::tbl(con, dbplyr::in_schema("nhp_modelling", "outpatients")) |>
     dplyr::inner_join(tbl_outpatients_diagnoses, by = c("attendkey")) |>
-    dplyr::filter(.data$is_tele_appointment == 0) |>
-    dplyr::group_by(.data$fyear, .data$procode3, .data$diagnosis, .data$is_surgical_specialty, .data$is_adult) |>
-    dplyr::summarise(n = dplyr::n(), .groups = "drop_last") |>
-    dplyr::mutate(p = .data$n * 1.0 / sum(.data$n, na.rm = TRUE)) |>
-    dbplyr::window_order(dplyr::desc(.data$n)) |>
-    dplyr::filter(
-      dplyr::row_number() <= 6,
-      .data$n >= 5
-    ) |>
-    dplyr::ungroup() |>
-    dplyr::collect() |>
+    dplyr::group_by(
+      .data$fyear,
+      .data$procode3,
+      .data$diagnosis,
+      .data$is_surgical_specialty,
+      .data$is_adult
+    )
+
+  list(
+    tbl_outpatients |>
+      dplyr::filter(.data$is_tele_appointment == 0) |>
+      dplyr::mutate(is_first = 1 - .data$is_first) |>
+      dplyr::summarise(
+        dplyr::across(c("is_first", "is_cons_cons_ref"), sum, na.rm = TRUE),
+        .groups = "keep"
+      ) |>
+      tidyr::pivot_longer(c("is_first", "is_cons_cons_ref"), values_to = "n") |>
+      dplyr::group_by(.data$name, .add = TRUE),
+    # need to handle tele appointments separately
+    tbl_outpatients |>
+      dplyr::filter(.data$is_tele_appointment == 1) |>
+      dplyr::summarise(
+        name = "is_tele_appointment",
+        n = dplyr::n(),
+        .groups = "keep"
+      )
+  ) |>
+    purrr::map_dfr(\(.x) {
+      .x |>
+        dbplyr::window_order(dplyr::desc(.data$n)) |>
+        dplyr::filter(
+          dplyr::row_number() <= 6,
+          .data$n >= 5
+        ) |>
+        dplyr::ungroup() |>
+        dplyr::collect()
+    }) |>
     janitor::clean_names() |>
     dplyr::rename(procode = "procode3") |>
     dplyr::mutate(
+      p = .data$n / sum(.data$n),
       subgroup = paste0(
         ifelse(.data$is_adult, "adult", "child"),
         "_",
@@ -94,7 +110,7 @@ get_op_diag_data <- function(provider_successors_last_updated) {
         "surgical"
       )
     ) |>
-    dplyr::inner_join(strategies, by = "subgroup") |>
+    dplyr::inner_join(strategies, by = c("name")) |>
     dplyr::transmute(
       .data$fyear,
       .data$procode,
@@ -107,29 +123,35 @@ get_op_diag_data <- function(provider_successors_last_updated) {
 
 
 get_op_age_sex_data <- function(op_data) {
-  strategies <- purrr::cross_df(
-    list(
-      strategy = c(
-        "convert_to_tele",
-        "consultant_to_consultant_reduction",
-        "followup_reduction"
-      ),
-      subgroup = c(
-        "adult_non-surgical",
-        "adult_surgical",
-        "child_surgical",
-        "child_non-surgical"
-      )
-    )
+  strategies <- tibble::tribble(
+    ~name, ~strategy,
+    "is_cons_cons_ref", "consultant_to_consultant_reduction",
+    "is_first", "followup_reduction",
+    "is_tele_appointment", "convert_to_tele"
   )
 
-  op_data |>
-    dplyr::filter(.data$is_tele_appointment == 0) |>
-    dplyr::count(.data$fyear, .data$age_group, .data$sex, procode = .data$procode3, .data$subgroup, wt = .data$n) |>
-    dplyr::inner_join(strategies, by = "subgroup") |>
+  dplyr::bind_rows(
+    op_data |>
+      dplyr::filter(.data$is_tele_appointment == 0) |>
+      dplyr::select(-"n") |>
+      tidyr::pivot_longer(c("is_first", "is_cons_cons_ref"), values_to = "n"),
+    op_data |>
+      dplyr::filter(.data$is_tele_appointment == 1) |>
+      dplyr::count(
+        .data$fyear,
+        .data$procode3,
+        .data$age_group,
+        .data$sex,
+        .data$subgroup,
+        wt = .data$n
+      ) |>
+      dplyr::mutate(name = "is_tele_appointment")
+  ) |>
+    dplyr::inner_join(strategies, by = "name") |>
+    dplyr::arrange(.data$age_group) |>
     dplyr::transmute(
       .data$fyear,
-      .data$procode,
+      procode = .data$procode3,
       strategy = glue::glue("{strategy}_({subgroup})"),
       dplyr::across("age_group", forcats::fct_inorder),
       .data$sex,
