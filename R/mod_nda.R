b48af88c7dc6dcb25b1603f31e0fcc73b6b5c159
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-06 13:18:02 +0000

    stores the slider values as a separate reactive values

--- R/mod_nda.R
+++ R/mod_nda.R
@@ -1,7 +1,7 @@
 age_bands <- function() {
   c(
-    "00-04",
-    "05-14",
+    " 0- 4",
+    " 5-14",
     "15-34",
     "35-49",
     "50-64",
@@ -20,30 +20,31 @@ age_bands <- function() {
 #'
 #' @importFrom shiny NS tagList
 mod_nda_ui <- function(id) {
-  ns <- NS(id)
+  ns <- shiny::NS(id)
   shiny::tagList(
-    tags$style(HTML(
-      "
-    .label-left .form-group {
-      display: flex;              /* Use flexbox for positioning children */
-      flex-direction: row;        /* Place children on a row (default) */
-      width: 100%;                /* Set width for container */
-      max-width: 400px;
-    }
+    shiny::tags$style(
+      shiny::HTML("
+        .label-left .form-group {
+          display: flex;              /* Use flexbox for positioning children */
+          flex-direction: row;        /* Place children on a row (default) */
+          width: 100%;                /* Set width for container */
+          max-width: 400px;
+        }
 
-    .label-left label {
-      margin-right: 2rem;         /* Add spacing between label and slider */
-      align-self: center;         /* Vertical align in center of row */
-      text-align: right;
-      flex-basis: 100px;          /* Target width for label */
-    }
+        .label-left label {
+          margin-right: 2rem;         /* Add spacing between label and slider */
+          align-self: center;         /* Vertical align in center of row */
+          text-align: right;
+          flex-basis: 100px;          /* Target width for label */
+        }
 
-    .label-left .irs {
-      flex-basis: 300px;          /* Target width for slider */
-    }
-    "
-    )),
-    shiny::selectInput(ns("activity_type"),
+        .label-left .irs {
+          flex-basis: 300px;          /* Target width for slider */
+        }
+      ")
+    ),
+    shiny::selectInput(
+      ns("activity_type"),
       label = "Activity Type",
       choices = c(
         "Non-Elective",
@@ -56,10 +57,10 @@ mod_nda_ui <- function(id) {
     purrr::map(
       age_bands(),
       \(.x) {
-        div(
+        shiny::tags$div(
           class = "label-left",
           shiny::sliderInput(
-            inputId = ns(.x),
+            inputId = ns(stringr::str_remove_all(.x, " ")),
             label = .x,
             min = 0,
             max = 200,
@@ -81,16 +82,23 @@ mod_nda_ui <- function(id) {
 #' @noRd
 mod_nda_server <- function(id, params) {
   shiny::moduleServer(id, function(input, output, session) {
-    shiny::observe({
-      params[["non-demographic_adjustment"]] <- purrr::map(
-        purrr::set_names(c("non-elective", "elective", "maternity")),
+    slider_values <- c("non-elective", "elective", "maternity") |>
+      purrr::set_names() |>
+      purrr::map(
         \(...) {
           purrr::map(
             purrr::set_names(age_bands()),
-            \(...) c(1, 1.2)
+            \(...) c(100, 120)
           )
         }
-      )
+      ) |>
+      (purrr::lift_dl(shiny::reactiveValues))()
+
+
+    shiny::observe({
+      params[["non-demographic_adjustment"]] <- slider_values |>
+        shiny::reactiveValuesToList() |>
+        purrr::map_depth(2, `/`, 100)
     })
 
     # when the activity_type input changes, update all of the sliders
@@ -103,8 +111,8 @@ mod_nda_server <- function(id, params) {
         \(.x) {
           shiny::updateSliderInput(
             session,
-            .x,
-            value = params[["non-demographic_adjustment"]][[at]][[.x]] * 100
+            stringr::str_remove_all(.x, " "),
+            value = slider_values[[at]][[.x]]
           )
         }
       )
@@ -115,24 +123,21 @@ mod_nda_server <- function(id, params) {
     purrr::walk(
       age_bands(),
       \(.x) {
+        i <- stringr::str_remove_all(.x, " ")
         shiny::observe({
           at <- shiny::req(input$activity_type)
 
-          params[["non-demographic_adjustment"]][[at]][[.x]] <- shiny::req(input[[.x]]) / 100
+          slider_values[[at]][[.x]] <- shiny::req(input[[i]])
+          params[["non-demographic_adjustment"]][[at]][[.x]] <- slider_values[[at]][[.x]] / 100
         }) |>
-          shiny::bindEvent(input[[.x]])
+          shiny::bindEvent(input[[i]])
       }
     )
 
     output$tmp <- shiny::renderPrint({
-      params[["non-demographic_adjustment"]] |>
+      slider_values |>
+        shiny::reactiveValuesToList() |>
         jsonlite::toJSON(pretty = TRUE, auto_unbox = TRUE)
     })
   })
 }
-
-## To be copied in the UI
-# mod_nda_ui("nda_1")
-
-## To be copied in the server
-# mod_nda_server("nda_1")
