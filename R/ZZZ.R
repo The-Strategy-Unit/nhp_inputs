56dc8295b9cbd95e333ca982d763045779d84119
Tom Jemmett <thomas.jemmett@nhs.net>
2023-05-04 08:47:42 +0100

    adds the theatres data to params object
    * statically implements the table using gt
    * hardcodes in data for now, this should be loaded at runtime based on
      dataset selected
    * allows user to set the baseline value, though this may be disabled
      using shiny js (tried to implement as an output, but this caused
      complications)

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -6,3 +6,9 @@ utils::globalVariables(c(
   "where", # source: https://github.com/r-lib/tidyselect/issues/201#issuecomment-650547846
   ".data", ".env"
 ))
+
+
+sanitize_input_name <- \(.x) .x |>
+  stringr::str_to_lower() |>
+  stringr::str_replace_all("(\\s|\\_|-)+", "-") |>
+  stringr::str_remove_all("[^a-z0-9-]+")
