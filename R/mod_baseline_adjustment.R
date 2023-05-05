b82e7ff7a5513d30f722ac34adfe54bc70cce435
Tom Jemmett <thomas.jemmett@nhs.net>
2023-05-05 21:41:15 +0100

    implements ui

--- R/mod_baseline_adjustment.R
+++ R/mod_baseline_adjustment.R
@@ -10,7 +10,104 @@
 mod_baseline_adjustment_ui <- function(id) {
   ns <- shiny::NS(id)
 
-  bs4Dash::box()
+  specs <- rtt_specialties() |>
+    dplyr::mutate(sanitized_code = sanitize_input_name(.data[["code"]]))
+
+  create_table <- function(at, g, df = specs) {
+    df |>
+      dplyr::mutate(
+        param = purrr::map(
+          .data[["sanitized_code"]],
+          \(.x) shinyjs::disabled(
+            shiny::sliderInput(
+              ns(glue::glue("param_{at}_{g}_{.x}")),
+              label = NULL,
+              min = 0,
+              max = 2,
+              value = c(0.95, 1.05),
+              step = 0.001
+            )
+          ) |>
+            as.character() |>
+            gt::html()
+        ),
+        include = purrr::map(
+          .data[["sanitized_code"]],
+          ~ shiny::checkboxInput(
+            ns(glue::glue("param_{at}_{g}_{.x}")),
+            label = NULL
+          ) |>
+            as.character() |>
+            gt::html()
+        )
+      ) |>
+      dplyr::select(-tidyselect::ends_with("code")) |>
+      gt::gt(rowname_col = "specialty") |>
+      gt::cols_label(
+        param ~ "Confidence Interval",
+        include ~ "Include?"
+      ) |>
+      gt::tab_options(table.width = gt::pct(100)) |>
+      gt::as_raw_html()
+  }
+
+  shiny::tagList(
+    shiny::tags$h1("Baseline Adjustment"),
+    bs4Dash::tabsetPanel(
+      shiny::tabPanel(
+        "Inpatients",
+        bs4Dash::tabsetPanel(
+          shiny::tabPanel(
+            "Elective",
+            create_table("ip", "elective")
+          ),
+          shiny::tabPanel(
+            "Non-Elective",
+            create_table("ip", "non-elective")
+          ),
+          shiny::tabPanel(
+            "Maternity",
+            create_table("ip", "maternity")
+          )
+        )
+      ),
+      shiny::tabPanel(
+        "Outpatients",
+        bs4Dash::tabsetPanel(
+          shiny::tabPanel(
+            "First Attendance",
+            create_table("op", "first")
+          ),
+          shiny::tabPanel(
+            "Follow-up Attendance",
+            create_table("op", "followup")
+          ),
+          shiny::tabPanel(
+            "Procedure",
+            create_table("op", "procedure")
+          )
+        )
+      ),
+      shiny::tabPanel(
+        "A&E",
+        create_table(
+          "aae",
+          "-",
+          tibble::tibble(code = c("ambulance", "walk-in")) |>
+            dplyr::mutate(
+              dplyr::across(
+                "code",
+                .fns = c(
+                  specialty = snakecase::to_title_case,
+                  sanitized_code = sanitize_input_name
+                ),
+                .names = "{.fn}"
+              )
+            )
+        )
+      )
+    )
+  )
 }
 
 #' baseline_adjustment Server Functions
