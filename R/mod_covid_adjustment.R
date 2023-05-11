4968d226f9ef127e904bea71c1714acf40d38e44
Tom Jemmett <thomas.jemmett@nhs.net>
2023-05-11 23:40:16 +0100

    prevents covid adjustment from running when year is not 201920

--- R/mod_covid_adjustment.R
+++ R/mod_covid_adjustment.R
@@ -18,13 +18,35 @@ mod_covid_adjustment_ui <- function(id) {
 mod_covid_adjustment_server <- function(id, params) {
   shiny::moduleServer(id, function(input, output, session) {
     covid_adjustment <- shiny::reactive({
+      if (params$start_year != "201920") {
+        return(
+          list(
+            "aae" = list(
+              "ambulance" = c(1.0, 1.0),
+              "walk-in" = c(1.0, 1.0)
+            ),
+            "ip" = list(
+              "elective" = c(1.0, 1.0),
+              "maternity" = c(1.0, 1.0),
+              "non-elective" = c(1.0, 1.0)
+            ),
+            "op" = list(
+              "first" = c(1.0, 1.0),
+              "followup" = c(1.0, 1.0),
+              "procedure" = c(1.0, 1.0)
+            )
+          )
+        )
+      }
+
       ds <- shiny::req(params$dataset)
+
 
       glue::glue("{ds}/covid_adjustment.rds") |>
         load_rds_from_adls() |>
         purrr::map_depth(2, `*`, 1 + c(-1, 1) * 0.025 / 12) # 5% either side, but adjusted to be for 1 month
     }) |>
-      shiny::bindCache(params$dataset)
+      shiny::bindCache(params$dataset, params$start_year)
 
     shiny::observe({
       params$covid_adjustment <- covid_adjustment()
