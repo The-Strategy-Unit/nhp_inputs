3a6b45a0c9b0ef7c9c5de6550d69f3ab66514c55
Tom Jemmett <thomas.jemmett@nhs.net>
2023-10-16 18:18:59 +0100

    fixes logic for what endpoint to select

--- R/fct_azure_storage.R
+++ R/fct_azure_storage.R
@@ -17,14 +17,14 @@ load_rds_from_adls <- function(file, inputs_data_version = Sys.getenv("NHP_INPUT
 #'
 #' @return the adls filesystem
 get_adls_fs <- function() {
-  ep_uri <- Sys.getenv("AZ_STORAGE_EP")
+  ep_uri <- Sys.getenv("LOCAL_STORAGE_EP")
 
-  ep <- if (ep_uri == "") {
-    ep_uri <- Sys.getenv("LOCAL_STORAGE_EP")
+  ep <- if (ep_uri != "") {
     sa_key <- Sys.getenv("LOCAL_STORAGE_KEY")
 
     AzureStor::adls_endpoint(ep_uri, key = sa_key)
   } else {
+    ep_uri <- Sys.getenv("AZ_STORAGE_EP")
     token <- AzureAuth::get_managed_token("https://storage.azure.com/") |>
       AzureAuth::extract_jwt()
 
