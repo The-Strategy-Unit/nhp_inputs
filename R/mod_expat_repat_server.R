7f95ec5a910db4f558bdc9f9516acfce2d59300d
Tom <thomas.jemmett@nhs.net>
2023-11-03 21:02:55 +0000

    cleans up expat repat code

--- R/mod_expat_repat_server.R
+++ R/mod_expat_repat_server.R
@@ -15,49 +15,45 @@ mod_expat_repat_server <- function(id, params, providers) { # nolint: object_usa
     rtt_specialties <- readRDS(app_sys("app", "data", "rtt_specialties.Rds"))
     icb_boundaries <- sf::read_sf(app_sys("app", "data", "icb_boundaries.geojson"))
 
+    # helpers ----
+    extract_expat_repat_data <- function(k) {
+      at <- shiny::req(input$activity_type)
+      st <- shiny::req(input$ip_subgroup)
+      t <- shiny::req(input$type)
+
+      expat_repat_data()[[k]][[at]] |>
+        dplyr::filter(
+          if (at == "ip") .data$admigroup == st else TRUE,
+          if (at == "aae") .data$is_ambulance == (t == "ambulance") else .data$specialty == t
+        )
+    }
+
     # reactives ----
+
+    # load all of the expat repat data for this dataset
     expat_repat_data <- shiny::reactive({
       ds <- shiny::req(params$dataset)
       load_rds_from_adls(glue::glue("{ds}/expat_repat.rds"))
     }) |>
       shiny::bindCache(params$dataset)
 
+    # extract the expat data for the current selection
     expat <- shiny::reactive({
-      at <- shiny::req(input$activity_type)
-      st <- shiny::req(input$ip_subgroup)
-      t <- shiny::req(input$type)
-
-      expat_repat_data()$expat[[at]] |>
-        dplyr::filter(
-          if (at == "ip") .data$admigroup == st else TRUE,
-          if (at == "aae") .data$is_ambulance == (t == "ambulance") else .data$specialty == t
-        ) |>
+      extract_expat_repat_data("expat") |>
         dplyr::select("fyear", "n")
     })
 
+    # extract the repat local data for the current selection
     repat_local <- shiny::reactive({
-      at <- shiny::req(input$activity_type)
-      st <- shiny::req(input$ip_subgroup)
-      t <- shiny::req(input$type)
-
-      expat_repat_data()$repat_local[[at]] |>
-        dplyr::filter(
-          if (at == "ip") .data$admigroup == st else TRUE,
-          if (at == "aae") .data$is_ambulance == (t == "ambulance") else .data$specialty == t
-        ) |>
+      extract_expat_repat_data("repat_local") |>
         dplyr::select("fyear", "provider", "n", "pcnt")
     })
 
+    # extract the repat nonlocal data for the current selection
     repat_nonlocal <- shiny::reactive({
-      at <- shiny::req(input$activity_type)
-      st <- shiny::req(input$ip_subgroup)
-      t <- shiny::req(input$type)
-
-      expat_repat_data()$repat_nonlocal[[at]] |>
+      extract_expat_repat_data("repat_nonlocal") |>
         dplyr::filter(
-          .data$fyear > 201415, # DQ issue: no rows prior to 15/16 have CCG
-          if (at == "ip") .data$admigroup == st else TRUE,
-          if (at == "aae") .data$is_ambulance == (t == "ambulance") else .data$specialty == t
+          .data$fyear > 201415 # DQ issue: no rows prior to 15/16 have CCG
         ) |>
         dplyr::group_by(dplyr::across(-c("n", "icb22cdh"))) |>
         dplyr::mutate(pcnt = .data$n / sum(.data$n)) |>
@@ -65,6 +61,7 @@ mod_expat_repat_server <- function(id, params, providers) { # nolint: object_usa
         dplyr::select("fyear", "icb22cdh", "n", "pcnt")
     })
 
+    # calculate the split between local and non-local activity by year
     repat_local_nonlocal_split <- shiny::reactive({
       repat_local() |>
         dplyr::filter(.data$provider == params$dataset) |>
@@ -92,53 +89,52 @@ mod_expat_repat_server <- function(id, params, providers) { # nolint: object_usa
     # two reactiveValues to keep track of the slider values
     # shadow_params always stores a value for each item selectable by the dropdowns
     # params contains the returned values, and will contain the value from shadow_params if "include" is checked
+    # this way, we can keep track of where someone set a slider to, even if they then decide to not include it
     shadow_params <- shiny::reactiveValues()
 
     # observers ----
 
-    # update values when a file is uploaded
+    # when the module is initialised, load the values from the loaded params file
     init <- shiny::observe(
       {
         p <- shiny::isolate({
           params
         })
 
-        shadow_params[["expat"]] <- params[["expat"]]
-        shadow_params[["repat_local"]] <- params[["repat_local"]]
-        shadow_params[["repat_nonlocal"]] <- params[["repat_nonlocal"]]
-
         # update the selected time profile (all 3 will have the same value, so just use expat)
         update_time_profile(p$time_profile_mappings[["expat"]])
 
-        tidyr::expand_grid(
-          type = names(shadow_params),
-          specialty = rtt_specialties
-        ) |>
-          purrr::pwalk(\(type, specialty) {
-            purrr::walk(c("elective", "non-elective", "maternity"), \(.y) {
-              v <- p[[type]]$ip[[.y]][[specialty]]
-              shadow_params[[type]]$ip[[.y]][[specialty]] <- v %||% c(0.95, 1.0)
-              params[[type]]$ip[[.y]][[specialty]] <- v
-            })
-            v <- p[[type]]$op[[specialty]]
-            shadow_params[[type]]$op[[specialty]] <- v %||% c(0.95, 1.0)
-            params[[type]]$op[[specialty]] <- v
-          })
+        default_values <- list(
+          expat = c(0.95, 1.0),
+          repat_local = c(1.0, 1.05),
+          repat_nonlocal = c(1.0, 1.05)
+        )
 
-        tidyr::expand_grid(
-          type = names(shadow_params),
-          arrival_mode = names(shadow_params$expat$aae)
+        # copy the values of the params to shadow params
+        c(
+          tidyr::expand_grid(
+            type = c("expat", "repat_local", "repat_nonlocal"),
+            activity_type = list(
+              list(
+                c("ip", "elective"),
+                c("ip", "non-elective"),
+                c("ip", "maternity"),
+                "op"
+              )
+            ),
+            specialty = rtt_specialties
+          ) |>
+            tidyr::unnest("activity_type") |>
+            purrr::pmap(purrr::compose(unname, c)),
+          list(c("expat", "aae", "ambulance")),
+          list(c("expat", "aae", "walk-in"))
         ) |>
-          purrr::pwalk(\(type, arrival_mode) {
-            v <- p[[type]]$aae[[arrival_mode]]
-            shadow_params[[type]]$aae[[arrival_mode]] <- v %||% c(0.95, 1.0)
-            params[[type]]$aae[[arrival_mode]] <- v
+          purrr::walk(\(.x) {
+            # if a value does exist in the params fallback to the default values
+            # for that type
+            v <- purrr::pluck(p, !!!.x) %||% default_values[[.x[[1]]]]
+            purrr::pluck(shadow_params, !!!.x) <- v
           })
-
-        default_spec <- rtt_specialties[[1]]
-        shiny::updateSelectInput(session, "activity_type", selected = "ip")
-        shiny::updateSelectInput(session, "ip_subgroup", selected = "elective")
-        shiny::updateSelectInput(session, "type", selected = default_spec)
 
         init$destroy()
       },
@@ -160,11 +156,22 @@ mod_expat_repat_server <- function(id, params, providers) { # nolint: object_usa
         )
       } else {
         type_label <- "Specialty"
-        type_values <- rtt_specialties
+        # if the subgroup is maternity, just show the one specialty
+        type_values <- if (at == "ip" && input$ip_subgroup == "maternity") {
+          "Other (Medical)"
+        } else {
+          rtt_specialties
+        }
       }
       shiny::updateSelectInput(session, "type", type_label, type_values)
+
+      # reset the subgroup selection if we aren't on inpatients
+      if (at != "ip") {
+        shiny::updateSelectInput(session, "ip_subgroup", selected = "elective")
+      }
     }) |>
-      shiny::bindEvent(input$activity_type)
+      shiny::bindEvent(input$activity_type, input$ip_subgroup)
+
 
     # watch for changes to the dropdowns
     # update the sliders to the values for the combination of the drop downs in shadow_params
@@ -193,6 +200,7 @@ mod_expat_repat_server <- function(id, params, providers) { # nolint: object_usa
     }) |>
       shiny::bindEvent(input$activity_type, input$ip_subgroup, input$type)
 
+    # set up the observers for the sliders/checkboxes
     purrr::walk(
       c("expat", "repat_local", "repat_nonlocal"),
       \(type) {
@@ -212,7 +220,6 @@ mod_expat_repat_server <- function(id, params, providers) { # nolint: object_usa
 
           if (at == "ip") {
             shadow_params[[type]][[at]][[st]][[t]] <- v
-
             params[[type]][[at]][[st]][[t]] <- if (include) v
           } else {
             shadow_params[[type]][[at]][[t]] <- v
@@ -241,7 +248,8 @@ mod_expat_repat_server <- function(id, params, providers) { # nolint: object_usa
         input$include_repat_local,
         input$repat_local,
         params$start_year,
-        "Percentange of ICB's activity Delivered by this Provider"
+        "Percentange of ICB's activity Delivered by this Provider",
+        scale = 10
       )
     })
 
@@ -267,10 +275,11 @@ mod_expat_repat_server <- function(id, params, providers) { # nolint: object_usa
 
       mod_expat_repat_trend_plot(
         df,
-        input$include_repat_local,
-        input$repat_local,
+        input$include_repat_nonlocal,
+        input$repat_nonlocal,
         params$start_year,
-        "Percentange of Non-Local ICBs Activity"
+        "Percentange of Non-Local ICBs Activity",
+        scale = 100
       )
     })
 
