8a4a59cba671e3999c2a583e3e829561d4297fad
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2023-11-28 16:20:31 +0000

    Set NDA values in server

--- R/mod_non_demographic_adjustment_server.R
+++ R/mod_non_demographic_adjustment_server.R
@@ -1,40 +1,46 @@
 #' Non Demographic Server Functions
 #'
 #' @noRd
-mod_non_demographic_adjustment_server <- function(id, params) { # nolint: object_usage_linter.
-  selected_time_profile <- update_time_profile <- NULL
-  c(selected_time_profile, update_time_profile) %<-% mod_time_profile_server(
-    shiny::NS(id, "time_profile"),
-    params
-  )
-
-  mod_reasons_server(shiny::NS(id, "reasons"), params, "non-demographic_adjustment")
-
-  nda_values <- purrr::imap(
-    nda_groups,
-    \(.x, .i) {
-      purrr::map(
-        purrr::set_names(names(.x$values)),
-        \(.y) mod_non_demographic_adjustment_input_server(id, .y, .i, params)
-      )
-    }
-  )
-
+mod_non_demographic_adjustment_server <- function(id, params) {
   shiny::moduleServer(id, function(input, output, session) {
-    shiny::observe({
-      params[["non-demographic_adjustment"]] <- nda_values |>
-        purrr::map(\(.x) {
-          .x |>
-            purrr::map(rlang::exec) |>
-            purrr::keep("include") |>
-            purrr::map("values")
-        })
+    # reactives ----
+
+    # load the non-demographic adjustment values
+    non_demographic_adjustment <- shiny::reactive({
+
+      return(
+        list(
+          "aae" = list(
+            "ambulance" = c(1.0117592, 1.014443),
+            "walk-in" = c(1.0117592, 1.014443)
+          ),
+          "ip" = list(
+            "elective" = c(1.0050266, 1.007375),
+            "maternity" = c(0.9732562, 0.980959),
+            "non-elective" = c(1.0187719, 1.024636)
+          ),
+          "op" = list(
+            "first" = c(1.0222222, 1.027585),
+            "followup" = c(1.0222222, 1.027585),
+            "procedure" = c(1.0222222, 1.027585)
+          )
+        )
+      )
+
     })
 
-    # update the time profile
+    # observers ----
+
+    # the non-demographic adjustment values are fixed, load them straight into the parameters
     shiny::observe({
-      params$time_profile_mappings[["non-demographic_adjustment"]] <- selected_time_profile()
-    }) |>
-      shiny::bindEvent(selected_time_profile())
+      params$non_demographic_adjustment <- non_demographic_adjustment()
+    })
+
+    # renders ----
+
+    # shows the selected values for the non-demographic adjustment
+    output$non_demographic_adjustment_table <- gt::render_gt({
+      mod_non_demographic_adjustment_table(non_demographic_adjustment())
+    })
   })
 }
