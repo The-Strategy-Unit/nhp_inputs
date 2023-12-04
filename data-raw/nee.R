b9e40294b9cc47ae542815892d8bd1f2a542907f
Chris Beeley <chris.beeley@gmail.com>
2023-12-04 17:15:19 +0000

    Flip the NEE results

--- data-raw/nee.R
+++ data-raw/nee.R
@@ -8,4 +8,10 @@ nee_lookup <- readr::read_csv("data-raw/NEE_lookup.csv")
 
 nee_table <- readr::read_csv("data-raw/NEE_table.csv")
 
+# flip the results to fit in with the rest of the dashboards- with 100%
+# representing no change and 0% representing total elimination
+
+nee_table <- nee_table |>
+  dplyr::mutate(dplyr::across(c(mean, percentile10, percentile90), ~ 100 - .x))
+
 usethis::use_data(nee_table, overwrite = TRUE)
