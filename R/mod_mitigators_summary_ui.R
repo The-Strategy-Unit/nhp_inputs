04800c32f73f7cabbb34784dd365a9c898065e59
Chris Beeley <chris.beeley@gmail.com>
2023-12-05 19:01:58 +0000

    Final mitigators table

--- R/mod_mitigators_summary_ui.R
+++ R/mod_mitigators_summary_ui.R
@@ -11,8 +11,27 @@ mod_mitigators_summary_ui <- function(id){
   ns <- NS(id)
   tagList(
 
-    gt::gt_output(ns("diagnoses_table"))
+    shiny::fluidRow(
 
+      col_4(
+        bs4Dash::box(
+          title = "Activity Mitigator",
+          width = 12,
+          shiny::p("This table summarises the most common healthcare activities
+                   across inpatients, outpatients, and A&E, to help prioritise
+                   the setting of mitigators. Some forms of activity are
+                   uncommon and so setting a mitigating factor will have
+                   relatively little influence over the model results")
+        )
+      ),
+
+      col_4(
+        bs4Dash::box(
+          width = 12,
+          gt::gt_output(ns("diagnoses_table"))
+        )
+      )
+    )
   )
 }
 
