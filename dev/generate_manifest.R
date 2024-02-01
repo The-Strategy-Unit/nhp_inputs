15ea80a5403967041fe855321008d666970b11f0
Tom Jemmett <thomas.jemmett@nhs.net>
2024-02-01 10:49:17 +0000

    updates generate_manifest and hook

--- dev/generate_manifest.R
+++ dev/generate_manifest.R
@@ -6,5 +6,11 @@ files <- c(
   fs::dir_ls("inst", recurse = TRUE, type = "file")
 )
 
-options(rsconnect.packrat = TRUE)
+# monkey patch the function to use our profile
+assignInNamespace(
+  "renvLockFile",
+  \(...) "renv/profiles/dev/renv.lock",
+  "rsconnect"
+)
+
 rsconnect::writeManifest(appFiles = files)
