09bbf23c1c57cef10f64abdbb323f7e781bbf06c
Tom Jemmett <thomas.jemmett@nhs.net>
2024-02-27 16:09:28 +0000

    disables day surgery

--- R/app_server.R
+++ R/app_server.R
@@ -64,7 +64,6 @@ app_server <- function(input, output, session) {
         "mitigators_mean_los_reduction",
         "mitigators_aec_los_reduction",
         "mitigators_preop_los_reduction",
-        "mitigators_bads",
         "mitigators_op_c2c_reduction",
         "mitigators_op_convert_tele",
         "mitigators_op_fup_reduction",
