881f34aa5d653ea3b1deaa6ea5fde90f20e89817
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-05-10 17:34:56 +0100

    Change variant 1 to variant 2 in variant detection function

--- R/mod_non_demographic_adjustment_utils.R
+++ R/mod_non_demographic_adjustment_utils.R
@@ -60,7 +60,7 @@ nda_groups <- list(
 
 detect_non_demographic_variant <- function(p_ndg, ndg_variants) {
 
-  detected_ndg_variant <- "variant_1"  # default
+  detected_ndg_variant <- "variant_2"  # default
 
   current_ndg_values <- unlist(p_ndg)  # NULL if new scenario
 
