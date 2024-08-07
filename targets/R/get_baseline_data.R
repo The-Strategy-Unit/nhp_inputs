dad5b545f71b266f2fe3ed3a9e13a2a934067d83
Tom Jemmett <thomas.jemmett@nhs.net>
2024-08-07 10:59:24 +0100

    updates baseline scripts to run on multiple years

--- targets/R/get_baseline_data.R
+++ targets/R/get_baseline_data.R
@@ -12,53 +12,58 @@ get_baseline_data <- function(all_providers, ...) {
     purrr::map(tibble::deframe)
 }
 
-get_baseline_data_ip <- function() {
-  providers <- fs::dir_ls("../nhp_model/data/2019")
+get_baseline_data_x <- function(file, fn) {
+  providers <- c(
+    fs::dir_ls("../nhp_model/data/2019"),
+    fs::dir_ls("../nhp_model/data/2022")
+  )
 
   providers |>
-    file.path("ip.parquet") |>
+    file.path(file) |>
     purrr::set_names(stringr::str_extract(providers, "(?<=/)\\w*$")) |>
     purrr::keep(file.exists) |>
     purrr::map(\(.x) {
+      fyear <- stringr::str_extract(.x, "(?<=/)\\d{4}(?=/)") |>
+        as.integer() |>
+        (\(.y) .y * 100 + (.y + 1) %% 100)()
+
       arrow::read_parquet(.x) |>
-        dplyr::count(fyear = 201920, .data[["group"]], .data[["tretspef"]])
-    }) |>
-    dplyr::bind_rows(.id = "procode3")
-}
-
-get_baseline_data_op <- function() {
-  providers <- fs::dir_ls("../nhp_model/data/2019")
-
-  providers |>
-    file.path("op.parquet") |>
-    purrr::set_names(stringr::str_extract(providers, "(?<=/)\\w*$")) |>
-    purrr::keep(file.exists) |>
-    purrr::map(\(.x) {
-      arrow::read_parquet(.x) |>
-        dplyr::summarise(
-          fyear = 201920,
-          n = sum(.data[["attendances"]] + .data[["tele_attendances"]]),
-          .by = c("group", "tretspef")
-        )
-    }) |>
-    dplyr::bind_rows(.id = "procode3")
-}
-
-get_baseline_data_aae <- function() {
-  providers <- fs::dir_ls("../nhp_model/data/2019")
-
-  providers |>
-    file.path("aae.parquet") |>
-    purrr::set_names(stringr::str_extract(providers, "(?<=/)\\w*$")) |>
-    purrr::keep(file.exists) |>
-    purrr::map(\(.x) {
-      arrow::read_parquet(.x) |>
-        dplyr::count(
-          fyear = 201920,
-          .data[["group"]],
-          wt = .data[["arrivals"]]
-        )
+        fn() |>
+        dplyr::mutate(fyear = fyear, .before = tidyselect::everything())
     }) |>
     purrr::discard(\(.x) nrow(.x) == 0) |>
     dplyr::bind_rows(.id = "procode3")
+}
+
+get_baseline_data_ip <- function() {
+  get_baseline_data_x(
+    "ip.parquet",
+    \(.x) dplyr::count(.x, .data[["group"]], .data[["tretspef"]])
+  )
+}
+
+get_baseline_data_op <- function() {
+  get_baseline_data_x(
+    "op.parquet",
+    \(.x) {
+      dplyr::summarise(
+        .x,
+        n = sum(.data[["attendances"]] + .data[["tele_attendances"]]),
+        .by = c("group", "tretspef")
+      )
+    }
+  )
+}
+
+get_baseline_data_aae <- function() {
+  get_baseline_data_x(
+    "aae.parquet",
+    \(.x) {
+      dplyr::count(
+        .x,
+        .data[["group"]],
+        wt = .data[["arrivals"]]
+      )
+    }
+  )
 }
