2e6b05d01c2beb19b406b86b35dfb1c278a9ec35
Tom Jemmett <thomas.jemmett@nhs.net>
2024-08-08 13:43:15 +0100

    adds new version to inputs selection app

--- deploy.R
+++ deploy.R
@@ -10,7 +10,7 @@ files <- files[!files == "deploy.R"]
 
 Sys.setenv(
   APP_VERSION_CHOICES = jsonlite::toJSON(
-    c("v2.0", "v1.2", "v1.1", "v1.0", "dev"),
+    c("v2.1", "v2.0", "v1.2", "v1.1", "v1.0", "dev"),
     auto_unbox = TRUE
   )
 )
