1a4e923b3e5cf98af6393c95c9912558a358207e
Tom Jemmett <thomas.jemmett@nhs.net>
2024-08-09 08:42:53 +0100

    converts deploy to a function

--- deploy.R
+++ deploy.R
@@ -1,26 +1,30 @@
-stopifnot("Need to run inside the inputs_selection_app folder" = file.exists("deploy.R"))
+deploy <- function() {
+  stopifnot("Need to run inside the inputs_selection_app folder" = file.exists("deploy.R"))
 
-files <- c(
-  dir(".", ".R"),
-  dir(".", ".yml"),
-  dir(".", "*.json"),
-  dir(".", "*.Rds"),
-  dir(".", "*.md")
-)
-
-files <- files[!files == "deploy.R"]
-
-Sys.setenv(
-  APP_VERSION_CHOICES = jsonlite::toJSON(
-    c("v2.1", "v2.0", "v1.2", "v1.1", "v1.0", "dev"),
-    auto_unbox = TRUE
+  files <- c(
+    dir(".", ".R"),
+    dir(".", ".yml"),
+    dir(".", "*.json"),
+    dir(".", "*.Rds"),
+    dir(".", "*.md")
   )
-)
 
-rsconnect::deployApp(
-  appId = 215,
-  appFiles = files,
-  envVars = c(
-    "APP_VERSION_CHOICES"
+  files <- files[!files == "deploy.R"]
+
+  Sys.setenv(
+    APP_VERSION_CHOICES = jsonlite::toJSON(
+      c("v2.1", "v2.0", "v1.2", "v1.1", "v1.0", "dev"),
+      auto_unbox = TRUE
+    )
   )
-)
+
+  rsconnect::deployApp(
+    appId = 215,
+    appFiles = files,
+    envVars = c(
+      "APP_VERSION_CHOICES"
+    )
+  )
+}
+
+deploy()
