a30a5b7b6a6c69d106fbeafc8b3e17c377169541
Tom Jemmett <thomas.jemmett@nhs.net>
2024-09-24 13:05:42 +0100

    adds upgrade from v2.1 to v2.2

--- app.R
+++ app.R
@@ -34,6 +34,11 @@ upgrade_params.v2.0 <- function(p) {
   upgrade_params(p)
 }
 
+upgrade_params.v2.1 <- function(p) {
+  class(p) <- p$app_version <- "v2.2"
+  upgrade_params(p)
+}
+
 params_path <- function(user, dataset) {
   path <- file.path(
     config::get("params_data_path"),
