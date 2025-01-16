19970fa3f1ff4f74ef7548b195b7012db81e6cbf
Tom Jemmett <thomas.jemmett@nhs.net>
2025-01-16 10:44:57 +0000

    removes targets pipeline
    moving to databricks (nhp_data)

--- targets/R/get_age_table.R
+++
@@ -1,7 +0,0 @@
-get_age_table <- function() {
-  tibble::tibble(age = 0:90) |>
-    dplyr::mutate(
-      age_group = cut_age(.data$age),
-      dplyr::across("age", ~ paste0(.x, ifelse(.x == max(.x), "+", "")))
-    )
-}
