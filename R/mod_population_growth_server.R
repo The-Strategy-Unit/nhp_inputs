e467c686be6b75719ed5c894e5d00f5326003c79
Tom Jemmett <thomas.jemmett@nhs.net>
2025-04-15 10:00:11 +0100

    adds ability to change the list of population projections displayed to a user
    resolves #451
    
    example of usage in golem config file:
    
    ``` yaml
    dataset_synthetic:
      population_projections:
        demo: Demo Projection
      subset:
        - demo
    ```
    
    this will now only show the "Demo Projection" slider for synthetic dataset. All other's will continue to see the default set of projections

--- R/mod_population_growth_server.R
+++ R/mod_population_growth_server.R
@@ -5,12 +5,9 @@ mod_population_growth_server <- function(id, params) {
   mod_reasons_server(shiny::NS(id, "reasons"), params, "demographic_factors")
 
   shiny::moduleServer(id, function(input, output, session) {
-    projections <- get_golem_config("population_projections")
-    changeable_projections <- names(projections)[-1]
+    projections <- get_population_growth_options(params$dataset)
 
-    stopifnot(
-      "Principal Projection should be the base case" = names(projections)[[1]] == "principal_proj"
-    )
+    changeable_projections <- names(projections)[-1]
 
     # when the module loads, run this observer once, and only once
     init <- shiny::observe(
@@ -40,7 +37,11 @@ mod_population_growth_server <- function(id, params) {
             \(.x, .i) shiny::updateSliderInput(session, .i, value = .x * 100)
           )
 
-          shiny::updateSliderInput(session, "principal_proj", value = (1 - sum(new_values)) * 100)
+          shiny::updateSliderInput(
+            session,
+            "principal_proj",
+            value = (1 - sum(new_values)) * 100
+          )
         }) |>
           shiny::bindEvent(input[[.x]])
       }
@@ -62,7 +63,9 @@ mod_population_growth_server <- function(id, params) {
       }
       v <- values()
 
-      params[["demographic_factors"]][["variant_probabilities"]] <- as.list(v[v > 0])
+      params[["demographic_factors"]][["variant_probabilities"]] <- as.list(v[
+        v > 0
+      ])
     }) |>
       shiny::bindEvent(values())
   })
