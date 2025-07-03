68133aed625f71adb2dcb8515aef314595e4ac77
Tom Jemmett <thomas.jemmett@nhs.net>
2025-07-03 14:50:17 +0100

    adds method to remove invalid mitigators
    uses the json schema to find any "additionalProperties" in the activity avoidance/efficiencies, and then removes them from the params object
    
    this will clean up mitigators that were added in earlier versions of the model but are no longer valid

---
+++ R/mod_run_model_remove_invalid_mitigators.R
@@ -0,0 +1,46 @@
+mod_run_model_remove_invalid_mitigators <- function(p, schema) {
+  json_p <- jsonlite::toJSON(p, auto_unbox = TRUE)
+
+  paths <- schema$validate(json_p, verbose = TRUE) |>
+    attr("errors") |>
+    dplyr::filter(
+      .data[["keyword"]] == "additionalProperties",
+      .data[["dataPath"]] |>
+        stringr::str_detect("/(activity_avoidance|efficiencies)/")
+    ) |>
+    dplyr::select("params", "dataPath") |>
+    tidyr::unnest_wider("params") |>
+    purrr::pmap(
+      \(dataPath, additionalProperty, ...) {
+        path <- stringr::str_split(dataPath, "/")[[1]][-1]
+        c(path, additionalProperty)
+      }
+    )
+
+  remove_item <- function(x, path) {
+    if (length(path) < 1) {
+      return(x)
+    }
+    if (length(path) == 1) {
+      x[[path]] <- NULL
+      return(x)
+    }
+
+    p1 <- path[[1]]
+    ps <- path[-1]
+    sublist <- x[[p1]]
+    if (!is.list(sublist)) {
+      return(x)
+    }
+
+    x[[p1]] <- remove_item(sublist, ps)
+
+    return(x)
+  }
+
+  for (i in paths) {
+    p <- remove_item(p, i)
+  }
+
+  p
+}
