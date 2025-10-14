f066e45e856459bf09f2f5cbb828cabeada8f951
Rhian Davies <thetrianglegirl@gmail.com>
2025-10-14 17:42:54 +0100

    ⬇️Add download button for provider's inequalities data

--- R/mod_inequalities_server.R
+++ R/mod_inequalities_server.R
@@ -4,5 +4,24 @@
 mod_inequalities_server <- function(id, params) {
   shiny::moduleServer(id, function(input, output, session) {
 
+
+    inequalities_data <- shiny::reactive({
+
+      dataset <- shiny::req(params$dataset)
+
+      load_inequalities_data() |>
+        dplyr::filter(
+          .data[["provider"]] == .env[["dataset"]]
+        )
+
+    })
+
+    output$download_inequalities <- shiny::downloadHandler(
+      filename = \() glue::glue("{params[['dataset']]}_inequalities.csv"),
+      content = \(file) {
+        readr::write_csv(inequalities_data(), file)
+      }
+    )
+
   })
 }
